f888161f35adfb0c27376d27cfe681c60848031e
= <=>
2024-08-24 19:40:02 +0530

    implemented map function to dynamically load notes from the notes.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import React from "react";
 import Footer from "./components/footer";
 import Header from "./components/header";
 import Note from "./components/note";
+import notes from "./notes";
+
+function createNote(note) {
+  return <Note key={note.key} title={note.title} content={note.content} />;
+}
 
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+      {notes.map(createNote)}
       <Footer />
     </div>
   );
--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -6,7 +6,7 @@ const year = date.getFullYear();
 function Footer() {
   return (
     <footer>
-      <p>copyright @{year}</p>
+      <p>copyright &copy; {year}</p>
     </footer>
   );
 }
